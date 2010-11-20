e1280f877ea7262b0c0ca0c391d0ec3859fbda58
Andris Reinman <andris@kreata.ee>
2010-11-20 12:28:30 +0200

    sign in, sign out - openID

--- main.py
+++ main.py
@@ -11,17 +11,75 @@ from django.utils import simplejson as json
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 import os
+import urllib
 
+
+
+
+def gen_template_values(http):
+    template_values = {
+        "user": users.get_current_user()
+    }
+    return template_values
+
+def showError(http, message=None):
+    template_values = gen_template_values(http)
+    template_values["message"] = message or "Error"
+    path = os.path.join(os.path.dirname(__file__), 'views/error.html')
+    http.response.out.write(template.render(path, template_values))
+
+def errorNotLoggedIn(http):
+    showError(http, "You need to be logged in!")
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'views/main.html')
+        template_values = gen_template_values(self)
+        path = os.path.join(os.path.dirname(__file__), 'views/front.html')
+        self.response.out.write(template.render(path, template_values))
+
+class LoggedHandler(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if not user:
+            return errorNotLoggedIn(self)
+        
+        template_values = gen_template_values(self)
+        path = os.path.join(os.path.dirname(__file__), 'views/logged.html')
+        self.response.out.write(template.render(path, template_values))
+
+class LogoutHandler(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if not user:
+            url = "/"
+        else:
+            url = users.create_logout_url("/")
+        self.redirect(url)
+    
+class LoginHandler(webapp.RequestHandler):
+    def get(self):
+        template_values = gen_template_values(self)
+        
+        target_url = self.request.get("target_url", None);
+        if target_url:
+            target_url = "/logged?target_url=%s" % urllib.quote_plus(target_url)
+        else:
+            target_url = "/logged"
+        
+        template_values["target_url"] = target_url
+        path = os.path.join(os.path.dirname(__file__), 'views/login.html')
         self.response.out.write(template.render(path, template_values))
     
+    def post(self):
+        url = users.create_login_url(self.request.get("target_url", None),
+                    federated_identity=self.request.get("openid", None))
+        self.redirect(url)
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+    application = webapp.WSGIApplication([('/', MainHandler),
+                                          ('/login', LoginHandler),
+                                          ('/logout', LogoutHandler),
+                                          ('/logged', LoggedHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
