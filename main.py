31f05be146807a40f8d87c0ac2ad0b3ebf56117f
Andris Reinman <andris@kreata.ee>
2010-11-24 18:39:02 +0200

    special chapters

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import diff_match_patch as dmp_module
 from django.utils import simplejson as json
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
+from django.template import Context, Template # for custom templates
+from django.template.loader import render_to_string
 import os
 import urllib
 import markdown2
@@ -31,6 +33,8 @@ class Book(db.Model):
   added = db.DateTimeProperty(auto_now_add = True)
   updated = db.DateTimeProperty(auto_now = True)
   title = db.StringProperty()
+  author = db.StringProperty()
+  isbn = db.StringProperty()
   use_index = db.BooleanProperty(default = True)
   use_toc = db.BooleanProperty(default = True)
   use_copyright = db.BooleanProperty(default = True)
@@ -95,6 +99,45 @@ def get_book(key, user_data=None):
 def get_chapters(book):
     return False
 
+def get_index_page(book):
+    
+    if book.page_index:
+        return book.page_index
+
+    template_values = {
+        "book": book,
+        "year": 2010
+    }
+    file = open('templates/index.html')
+    tmpl = file.read().decode("utf-8")
+    file.close()
+    return Template(tmpl.encode('utf-8')).render(Context(template_values))
+
+def get_toc_page(book):
+    
+    if book.page_toc:
+        return book.page_toc
+
+    template_values = {
+        "book": book
+    }
+    file = open('templates/toc.html')
+    tmpl = file.read().decode("utf-8")
+    file.close()
+    return Template(tmpl.encode('utf-8')).render(Context(template_values))
+
+def get_copyright_page(book):
+    
+    if book.page_copyright:
+        return book.page_copyright
+
+    template_values = {
+        "book": book
+    }
+    file = open('templates/copyright.html')
+    tmpl = file.read().decode("utf-8")
+    file.close()
+    return Template(tmpl.encode('utf-8')).render(Context(template_values))
 
 def gen_paging(page, pages, url):
     
@@ -133,6 +176,7 @@ class MainHandler(webapp.RequestHandler):
         
         f = open('gae.html', 'r')
         html = f.read().decode("utf-8")
+        f.close()
         
         html = html2markdown.html2text(html)
         html = markdown2.markdown(html)
@@ -176,8 +220,10 @@ class BookAddHandler(webapp.RequestHandler):
         
         template_values = gen_template_values(self)
         template_values["user_data"] = user_data
-        template_values["key"] = self.request.get("title", u"")
+        template_values["key"] = self.request.get("key", u"")
         template_values["title"] = self.request.get("title", u"")
+        template_values["author"] = self.request.get("author", u"")
+        template_values["isbn"] = self.request.get("isbn", u"")
         template_values["use_index"] = self.request.get("use_index", False)
         template_values["use_toc"] = self.request.get("use_toc", False)
         template_values["use_copyright"] = self.request.get("use_copyright", False)
@@ -201,6 +247,8 @@ class BookEditHandler(webapp.RequestHandler):
         template_values["user_data"] = user_data
         template_values["key"] = book.key()
         template_values["title"] = self.request.get("title", book.title)
+        template_values["author"] = self.request.get("author", book.author)
+        template_values["isbn"] = self.request.get("isbn", book.isbn)
         template_values["use_index"] = self.request.get("use_index", book.use_index)
         template_values["use_toc"] = self.request.get("use_toc", book.use_toc)
         template_values["use_copyright"] = self.request.get("use_copyright", book.use_copyright)
@@ -237,7 +285,9 @@ class BookSaveHandler(webapp.RequestHandler):
             return errorNotLoggedIn(self)
         
         key = self.request.get("key", False)
-        title = self.request.get("title","Untitled book")
+        title = self.request.get("title",u"Untitled book")
+        author = self.request.get("author",u"No author")
+        isbn = self.request.get("isbn",u"")
 
         use_index = bool(self.request.get("use_index"))
         use_toc = bool(self.request.get("use_toc"))
@@ -247,6 +297,8 @@ class BookSaveHandler(webapp.RequestHandler):
             book = Book(parent=user_data)
             book.user = user_data
             book.title = title
+            book.author = author
+            book.isbn = isbn
             
             book.use_index =  use_index
             book.use_toc = use_toc
@@ -264,6 +316,9 @@ class BookSaveHandler(webapp.RequestHandler):
             if not book:
                 return error404(self)
             book.title = title
+            book.author = author
+            book.isbn = isbn
+            
             book.use_index =  use_index
             book.use_toc = use_toc
             book.use_copyright = use_copyright
@@ -276,8 +331,6 @@ class BookSaveHandler(webapp.RequestHandler):
             db.run_in_transaction(save_book)
         
         self.redirect("/books")
-
-
 
 
 class ChapterListHandler(webapp.RequestHandler):
@@ -300,6 +353,94 @@ class ChapterListHandler(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
         
 
+class ChapterIndexHandler(webapp.RequestHandler):
+    def get(self):
+        user_data = get_user()
+        if not user_data:
+            return errorNotLoggedIn(self)
+
+        key = self.request.get("book", False)
+        book = get_book(key)
+        if not book:
+            return error404(self)
+        
+        index = get_index_page(book)
+        
+        template_values = gen_template_values(self)
+        template_values["book"] = book
+        template_values["contents"] = index
+        template_values["title"] = "Index page"
+        template_values["type"] = "index"
+        path = os.path.join(os.path.dirname(__file__), 'views/editor.html')
+        self.response.out.write(template.render(path, template_values))
+        
+class ChapterTOCHandler(webapp.RequestHandler):
+    def get(self):
+        user_data = get_user()
+        if not user_data:
+            return errorNotLoggedIn(self)
+
+        key = self.request.get("book", False)
+        book = get_book(key)
+        if not book:
+            return error404(self)
+        
+        toc = get_toc_page(book)
+        
+        template_values = gen_template_values(self)
+        template_values["book"] = book
+        template_values["contents"] = toc
+        template_values["title"] = "Table of contents"
+        template_values["type"] = "toc"
+        path = os.path.join(os.path.dirname(__file__), 'views/editor.html')
+        self.response.out.write(template.render(path, template_values))
+        
+class ChapterCopyrightHandler(webapp.RequestHandler):
+    def get(self):
+        user_data = get_user()
+        if not user_data:
+            return errorNotLoggedIn(self)
+
+        key = self.request.get("book", False)
+        book = get_book(key)
+        if not book:
+            return error404(self)
+        
+        copyright = get_copyright_page(book)
+        
+        template_values = gen_template_values(self)
+        template_values["book"] = book
+        template_values["contents"] = copyright
+        template_values["title"] = "Copyright page"
+        template_values["type"] = "copyright"
+        path = os.path.join(os.path.dirname(__file__), 'views/editor.html')
+        self.response.out.write(template.render(path, template_values))
+
+class ChapterSaveSpecialHandler(webapp.RequestHandler):
+    def post(self):
+        user_data = get_user()
+        if not user_data:
+            return errorNotLoggedIn(self)
+
+        key = self.request.get("book", False)
+        book = get_book(key)
+        if not book:
+            return error404(self)
+
+        type = self.request.get("type", False)
+        
+        if type=="index":
+            book.page_index = self.request.get("contents",u"")
+        elif type=="toc":
+            book.page_toc = self.request.get("contents",u"")
+        elif type=="copyright":
+            book.page_copyright = self.request.get("contents",u"")
+        else:
+            return error404(self)
+
+        book.put()
+        memcache.set("<book-%s>" % book.key(), book)
+        self.redirect("/chapters?key=%s" % book.key())
 
 class LoggedHandler(webapp.RequestHandler):
     def get(self):
@@ -350,7 +491,11 @@ def main():
                                           ('/book-edit', BookEditHandler),
                                           ('/book-save', BookSaveHandler),
                                           ('/book-remove', BookRemoveHandler),
-                                          ('/chapters', ChapterListHandler)],
+                                          ('/chapters', ChapterListHandler),
+                                          ('/chapter-index', ChapterIndexHandler),
+                                          ('/chapter-toc', ChapterTOCHandler),
+                                          ('/chapter-copyright', ChapterCopyrightHandler),
+                                          ('/chapter-save-special', ChapterSaveSpecialHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
